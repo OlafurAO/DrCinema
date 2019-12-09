0e3f8fa6fb99c9da8b609a2cf3e71f764da35127
Olafur <olafurao16@ru.is>
2019-12-09 12:03:48 +0000

    fix

--- App.js
+++ App.js
@@ -18,7 +18,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'grey',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
